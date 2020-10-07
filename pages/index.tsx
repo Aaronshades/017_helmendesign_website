50d785781536ee484492e11d6159c5e485d4fb60
Haakon Helmen <haakon.s.helmen@gmail.com>
2020-10-07 09:33:08 +0200

    #2 Adding description to document for better SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,28 @@
 import Head from "next/head";
+import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css";
 
-const Home = () => {
+const Home = ({ title, description, ...props }) => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Helmen Design</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Helmen Design</h1>
-      </main>
-
-      <footer className={styles.footer}></footer>
+      <Layout pageTitle={title} description={description}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Welcome to Helmen Design</h1>
+        </main>
+      </Layout>
     </div>
   );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const configData = await import(`../siteconfig.json`);
+
+  return {
+    props: {
+      title: configData.default.title,
+      description: configData.default.description,
+    },
+  };
+}
