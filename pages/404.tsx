eaffe04f01d45a1a6b31e7e93969410ece66d9a8
Haakon Helmen <haakon.s.helmen@gmail.com>
2021-01-16 17:46:39 +0100

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout/Layout";
 const Custom404 = () => {
   return (
     <Layout
-      pageTitle="Helmen Design PageNotFound"
+      pageTitle="Helmen Design Page Not Found"
       description="Page not found."
     >
       <h2>Ops! Our bad, this page is empty.</h2>
